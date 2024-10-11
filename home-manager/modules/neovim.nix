eb73d45a8d562db476a7c3caaa8f562cefb5cbed
Robert Menke <robert.b.menke@gmail.com>
2024-10-11 00:49:52 -0400

    use correct syntax for neovim config directory

--- home-manager/modules/neovim.nix
+++ home-manager/modules/neovim.nix
@@ -6,7 +6,8 @@
     vimAlias = true;
     vimdiffAlias = true;
     defaultEditor = true;
-    # Symlink ~/.dotfiles/nvim to ~/.config/nvim
-    xdg.configFile."nvim" = "$HOME/.dotfiles/nvim";
+    configFile = {
+      nvim = "$HOME/.dotfiles/nvim";
+    };
   };
 }
