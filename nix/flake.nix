75183221c18fa0bf444deb93a1e220b8fc793beb
Robert Menke <robert.b.menke@gmail.com>
2024-10-12 01:29:11 -0400

    separate darwin configuration

--- nix/flake.nix
+++ nix/flake.nix
@@ -4,17 +4,34 @@
   inputs = {
     # Specify the source of Home Manager and Nixpkgs.
     nixpkgs.url = "nixpkgs/release-24.05";
+    nix-darwin = {
+      url = "github:LnL7/nix-darwin";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
     home-manager = {
       url = "github:nix-community/home-manager/release-24.05";
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };
 
-  outputs = { nixpkgs, home-manager, ... }:
+  outputs = { self, nixpkgs, nix-darwin, home-manager, ... }:
     let
       system = "aarch64-darwin";
       pkgs = nixpkgs.legacyPackages.${system};
     in {
+      # Build darwin flake using:
+      # $ darwin-rebuild build --flake .#MacBook-Pro-60
+      darwinConfigurations."robert-mbp" = nix-darwin.lib.darwinSystem {
+        modules = [ ./darwin-configuration.nix ];
+        extraSpecialArgs = {
+            isDarwin = true;
+            isLinux = false;
+        };
+      };
+
+      # Expose the package set, including overlays, for convenience.
+      darwinPackages = self.darwinConfigurations."robert-mbp".pkgs;
+
       homeConfigurations."rbmenke" = home-manager.lib.homeManagerConfiguration {
         inherit pkgs;
 
