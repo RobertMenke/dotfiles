80758bc883a78f9bdfdd96d318046d0ba0d1a968
Robert Menke <robert.b.menke@gmail.com>
2024-10-17 23:08:33 -0400

    home manager version

--- nix/flake.nix
+++ nix/flake.nix
@@ -9,7 +9,7 @@
       inputs.nixpkgs.follows = "nixpkgs";
     };
     home-manager = {
-      url = "github:nix-community/home-manager/release-24.05";
+      url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };
