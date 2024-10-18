1ea982d11d2f8fa99b8f149164236d5465102b29
Robert Menke <robert.b.menke@gmail.com>
2024-10-17 23:09:19 -0400

    home manager version

--- nix/flake.nix
+++ nix/flake.nix
@@ -5,11 +5,11 @@
     # Specify the source of Home Manager and Nixpkgs.
     nixpkgs.url = "nixpkgs/release-24.05";
     nix-darwin = {
-      url = "github:LnL7/nix-darwin";
+      url = "github:LnL7/nix-darwin/release-24.05";
       inputs.nixpkgs.follows = "nixpkgs";
     };
     home-manager = {
-      url = "github:nix-community/home-manager";
+      url = "github:nix-community/home-manager/release-24.05";
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };
