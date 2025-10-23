ad3365516adfcff326f63156f8fa24c9044ea999
Robert Menke <robert.b.menke@gmail.com>
2025-10-22 22:58:39 -0400

    allow unfree

--- nix/darwin-configuration.nix
+++ nix/darwin-configuration.nix
@@ -60,6 +60,7 @@ in {
 
   # The platform the configuration will be used on.
   nixpkgs.hostPlatform = "aarch64-darwin";
+  nixpkgs.config.allowUnfree = true;
 
   fonts.packages = with pkgs; [ nerd-fonts.jetbrains-mono ];
 
